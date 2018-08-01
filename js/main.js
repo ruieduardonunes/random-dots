e616ef6fb9377ca0aac5e7292e5ff0ebfeca76dc
ruieduardonunes <github@ruieduardo.com>
2018-08-01 15:53:36 +0200

    new version

--- js/main.js
+++ js/main.js
@@ -1,45 +1,114 @@
-function getWord() {
-  var array = [];
-  var xmlhttp;
-  if (window.XMLHttpRequest) {
-    // code for IE7+, Firefox, Chrome, Opera, Safari
-    xmlhttp = new XMLHttpRequest();
-  } else {
-    // code for IE6, IE5
-    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-  }
-  xmlhttp.onreadystatechange = function() {
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-      var text = xmlhttp.responseText;
-      // Now convert it into array using regex
-      array = text.split(/\n|\r\s|\s|[, ]+/g);
-      var word = array[Math.floor(Math.random() * array.length)];
+var plays = 0;
+var words = true;
 
-      document.getElementById("word").textContent = word;
-    }
-  };
-  xmlhttp.open("GET", "/random-dots/data/words.txt", true);
-  xmlhttp.send();
+function getWord() {
+	var array = [];
+	var xmlhttp;
+	if (window.XMLHttpRequest) {
+		// code for IE7+, Firefox, Chrome, Opera, Safari
+		xmlhttp = new XMLHttpRequest();
+	} else {
+		// code for IE6, IE5
+		xmlhttp = new ActiveXObject('Microsoft.XMLHTTP');
+	}
+	xmlhttp.onreadystatechange = function() {
+		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+			var text = xmlhttp.responseText;
+			// Now convert it into array using regex
+			array = text.split(/\n|\r\s|\s|[, ]+/g);
+			var word = array[Math.floor(Math.random() * array.length)];
+
+			document.getElementById('word').textContent = word;
+			plays = plays + 1;
+
+			var interjections = [
+				'uh! this is a nice one',
+				'ahh... Inspiring!',
+				'Ooh-la-la',
+				'Whoa',
+				'Oh dear!',
+				'Awww',
+				'bingo!',
+				'gosh',
+				'Touché',
+				'one more!',
+				'nailed it!',
+				'thats awkward',
+				'tasty!',
+				'one more!',
+				'one more!',
+				'one more!',
+				'one more!',
+				'one more!',
+				'one more!',
+				'one more!',
+				'one more!',
+				'one more!',
+				'one more!',
+				'one more!',
+				'one more!',
+				'one more!'
+			];
+
+			var newInterjection = Math.random() >= 0.5;
+			var rand =
+				interjections[Math.floor(Math.random() * interjections.length)];
+
+			console.log(newInterjection);
+			console.log(rand);
+
+			if (plays == 0) {
+				// do nothing
+			} else if (plays == 1) {
+				document.getElementById('play').textContent = 'click for a new word';
+			} else if (newInterjection) {
+				document.getElementById('play').textContent = rand;
+			} else {
+				document.getElementById('play').textContent = 'one more!';
+			}
+		}
+	};
+	xmlhttp.open('GET', 'data/words.txt', true);
+	xmlhttp.send();
+}
+
+function playToggle() {
+	var wordWrapper = document.getElementById('word-wrapper');
+	var imageWrapper = document.getElementById('image-wrapper');
+
+	var toggleButton = document.getElementById('toggleButton');
+	var playButton = document.getElementById('play');
+
+	if (words) {
+		wordWrapper.style.display = 'none';
+		imageWrapper.style.display = 'flex';
+		toggleButton.textContent = 'play with words';
+		playButton.innerHTML = 'click for a new image';
+		playButton.onclick = reloadImage;
+		reloadImage();
+		plays = 0;
+		words = false;
+	} else {
+		wordWrapper.style.display = 'flex';
+		imageWrapper.style.display = 'none';
+		toggleButton.textContent = 'play with images';
+		playButton.innerHTML = 'click for a new word';
+		playButton.onclick = getWord;
+		plays = 0;
+		words = true;
+	}
 }
 
 window.onload = getWord();
 
-function showLearn() {
-  var wrapper = document.getElementById("learn");
-  var toggleButton = document.getElementById("toggleButton");
+function reloadImage() {
+	var image = document.getElementById('hero-image');
 
-  if (wrapper.style.top == "0px") {
-    wrapper.style.top = "102vh";
-    toggleButton.innerHTML = "learn";
-  } else {
-    wrapper.style.top = "0";
-    console.log(wrapper.style.top);
-    toggleButton.innerHTML = "close";
-  }
+	image.src = 'https://source.unsplash.com/1600x900/?places,life';
 }
 
-function reloadImage() {
-  var image = document.getElementsByClassName("hero-img")[0];
-
-  image.src = "https://source.unsplash.com/1600x900/?places,life";
+function scrollLearn() {
+	document.querySelector('#learn-wrapper').scrollIntoView({
+		behavior: 'smooth'
+	});
 }
